8182d32d65551562dcd9f58798b3218c80dff4de
Arjun <arjunsinghanya35@gmail.com>
2026-07-13 19:26:58 +0530

    again image path are setted

--- src/pages/about/component/top/Top.jsx
+++ src/pages/about/component/top/Top.jsx
@@ -4,7 +4,7 @@ import './Top.css'
 function About() {
   return (
     <div className="about-container">
-      <img src={"/about/background images.png"} alt="Background" />
+      <img src={"/about_images/background images.png"} alt="Background" />
       <h1>About Shabdd Technologies</h1>
       <p>Your trusted partner for innovative IT solutions designed to empower your business.</p>
     </div>
